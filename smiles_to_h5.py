10e929430f70717d768463274bfbc0351d033e2a
klopezperez <klopezperez@ufl.edu>
2024-02-08 13:46:54 -0500

    h5 generation updated

--- smiles_to_h5.py
+++ smiles_to_h5.py
@@ -4,14 +4,15 @@
     Output: H5 file with fingerprints"""
 
 ### Imports ###
-import numpy as np
-from FPSim2.io import create_db_file
 import argparse 
+from FPSim2.io import create_db_file
+import numpy as np
 import pandas as pd
+import pickle
 
 ### Parsing arguments ###
 args = argparse.ArgumentParser(description='Convert SMILES to H5 fingerprints')
-args.add_argument('-i', '--input', help='Input csv file with smiles', required=True)
+args.add_argument('-i', '--input', help='Input file with smiles', required=True)
 args.add_argument('-c', '--column', help='Column with smiles', required=True)
 args.add_argument('-fp', '--fingerprint', help='Fingerprint type', required=True)
 args.add_argument('-r', '--radius', help='Radius for Morgan fingerprints', required=False, default=2, type=int)
@@ -19,11 +20,15 @@ args.add_argument('-b', '--bits', help='Number of bits for Morgan fingerprints',
 args.add_argument('-o', '--output', help='Output H5 file', required=True)
 args = args.parse_args()
 
-### Read csv input file ###
-df = pd.read_csv(args.input, sep=',')
-smiles = df[args.column].tolist()
-#ids = df['id'].tolist()
-#fpsim2list = [[ids[i], smiles[i]] for i in range(len(smiles))]
+### Read input file ###
+if args.input.endswith('.csv'):
+    data = pd.read_csv(args.input)
+    smiles = data[args.column].tolist()
+elif args.input.endswith('.pkl'):
+    file = open(args.input, 'rb')
+    data = pickle.load(file)
+    file.close()
+    smiles = data[args.column]
 
 ### Convert smiles to H5 fingerprints ###
 if args.fingerprint != 'Morgan':
