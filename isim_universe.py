0e3a67158167fccd6d8ef700afd092ae7e5830c0
klopezperez <klopezperez@ufl.edu>
2024-02-19 16:09:00 -0500

    isim_universe_parser

--- isim_universe.py
+++ isim_universe.py
@@ -1,11 +1,16 @@
+import argparse
 from galaxy_class import *
 import glob
 import pandas as pd
-import pickle
+
+# Create a parser
+parser = argparse.ArgumentParser(description = 'iSIM universe calculations')
+parser.add_argument('-d', '--directory', help = 'Directory containing the h5 files', required = True)
+args = parser.parse_args()
 
 # Get the isim for each of the databases
 isim_universe = []
-for file in glob.glob('*.h5'):
+for file in glob.glob(args.directory + '*.h5'):
     G = ChemGalaxy(file.split('/')[-1][:-3], file)
     isim_universe.append([G.name, G.size, G.isim, G.get_outliers(), G.get_medoids(), G.get_isim(G.get_outliers()), G.get_isim(G.get_medoids()), G.get_ctotal(G.get_outliers()), G.get_ctotal(G.get_medoids())])
 
