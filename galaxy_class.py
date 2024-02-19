6803ee7620ee276e78525c29121dbee12616a90a
klopezperez <klopezperez@ufl.edu>
2024-02-19 14:29:14 -0500

    update_galaxy

--- galaxy_class.py
+++ galaxy_class.py
@@ -11,7 +11,7 @@ class ChemGalaxy:
         self.n_ary = n_ary
         self.fingerprints = unpack_fpe(self.fpe)
         self.size = len(self.fingerprints)
-        self.indexes = self.fpe.fps[:, 0]
+        self.indexes = np.array(self.fpe.fps[:, 0]) - 1
         self.isim = calculate_isim(self.fingerprints, n_objects=self.size, n_ary=n_ary)
         self.comp_isim = None
 
@@ -28,28 +28,42 @@ class ChemGalaxy:
         # Get the indexes of the outliers (mols with highest comp_isim values)
         if self.comp_isim is None: self.calc_comp_isim()
         num_outliers = int(self.size * percentage)
-        threshold = np.partition(self.comp_isim, -num_outliers)[-num_outliers]
-        self.outliers = self.indexes[self.comp_isim > threshold]
+        comp_sim = [(index, value) for index, value in zip(self.indexes, self.comp_isim)]
+        comp_sim.sort(key = lambda x: x[1])
+        self.outliers = [index for index, value in comp_sim[-num_outliers:]]
         return self.outliers
     
     def get_medoids(self, percentage = 0.05):
         # Get the indexes of the medoids (mols with lowest comp_isim values)
         if self.comp_isim is None: self.calc_comp_isim()
         num_medoids = int(self.size * percentage)
-        threshold = np.partition(self.comp_isim, num_medoids)[num_medoids]
-        self.medoids = self.indexes[self.comp_isim < threshold]
+        comp_sim = [(index, value) for index, value in zip(self.indexes, self.comp_isim)]
+        comp_sim.sort(key = lambda x: x[1])
+        self.medoids = [index for index, value in comp_sim[:num_medoids]]
         return self.medoids
+    
+    def get_comp_isims(self, indexes_list = None):
+        if self.comp_isim is None: self.calc_comp_isim()
+        if indexes_list is None: indexes_list = self.indexes
+        indexes_list = np.array(indexes_list)
+        indexes_list = np.where(np.isin(self.indexes, indexes_list))[0]
+        return self.comp_isim[indexes_list]
     
     def get_fingerprints(self, indexes_list = None):
         if indexes_list is None: indexes_list = self.indexes
+        indexes_list = np.array(indexes_list)
+        indexes_list = np.where(np.isin(self.indexes, indexes_list))[0]
         return self.fingerprints[indexes_list]
     
     def get_ctotal(self, indexes_list = None):
         if indexes_list is None: indexes_list = self.indexes
+        indexes_list = np.array(indexes_list)
+        indexes_list = np.where(np.isin(self.indexes, indexes_list))[0]
         return np.sum(self.fingerprints[indexes_list], axis = 0)
     
     def get_isim(self, indexes_list):
         if indexes_list is None: return self.isim
+        if type(indexes_list) == int: raise ValueError('Indexes_list must be a list of indexes, isim is not calculated for a single molecule.')
         else: return calculate_isim(self.fingerprints[indexes_list], n_objects=len(indexes_list), n_ary=self.n_ary)
 
 
